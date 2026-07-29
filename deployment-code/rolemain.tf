fc1ab7672772f02bafb892a09debc1076a7e9568
Raj <ishrajcodes@gmail.com>
2026-07-29 16:49:14 +0200

    10-updated_logingestion-platform-mg

--- deployment-code/rolemain.tf
+++ deployment-code/rolemain.tf
@@ -1,10 +1,10 @@
 module "plat_log_ingestion_policy_identity_role" {
-  source = "git::https://api.github.com/repos/Azure/terraform-azurerm-avm-res-authorization-roleassignment/git/commits/8f90a2110e81def1c63f8f81f44e60554793e50e"
+  source = "git::https://github.com/Azure/terraform-azurerm-avm-res-authorization-roleassignment.git?ref=8f90a2110e81def1c63f8f81f44e60554793e50e"
 
   role_assignments_azure_resource_manager = {
-    variable.plat_log_ingestion_policy_identity_rolename = {
+    (var.plat_log_ingestion_policy_identity_rolename) = {
       principal_object_id = azurerm_management_group_policy_assignment.activity_log_to_law.identity[0].principal_id
-      role_id             = variable.plat_log_ingestion_policy_identity_roleid
+      role_id             = var.plat_log_ingestion_policy_identity_roleid
       scope               = azurerm_management_group.platform_landing_zone.id
     }
   }
